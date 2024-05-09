913ed012bba4e045e48ae64f2335235dc93f277b
HuzaifaJabbar17 <jabbarhuzaifa123@gmail.com>
2024-05-09 10:55:24 +0530

    handling signup form data

--- src/app/_components/RestaurantSignUp.js
+++ src/app/_components/RestaurantSignUp.js
@@ -1,4 +1,18 @@
+import { useState } from "react";
+
 const RestaurantSignUp = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [city, setCity] = useState("");
+  const [address,setAddress] = useState("");
+  const [contact, setContact] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSignUp =() =>{
+    console.log(email,password,confirmPassword,name,city,address,contact);
+  }
+
   return (
     <>
       <div>
@@ -10,6 +24,8 @@ const RestaurantSignUp = () => {
               type="text"
               placeholder="enter email id"
               className="input-field"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
             ></input>
           </div>
 
@@ -18,6 +34,8 @@ const RestaurantSignUp = () => {
               type="text"
               placeholder="enter restaurant name"
               className="input-field"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
             ></input>
           </div>
 
@@ -26,6 +44,8 @@ const RestaurantSignUp = () => {
               type="test"
               placeholder="enter city"
               className="input-field"
+              value={city}
+              onChange={(event) => setCity(event.target.value)}
             ></input>
           </div>
 
@@ -34,6 +54,8 @@ const RestaurantSignUp = () => {
               type="text"
               placeholder="enter address"
               className="input-field"
+              value={address}
+              onChange={(event) => setAddress(event.target.value)}
             ></input>
           </div>
 
@@ -42,6 +64,8 @@ const RestaurantSignUp = () => {
               type="number"
               placeholder="enter phone number"
               className="input-field"
+              value={contact}
+              onChange={(event) => setContact(event.target.value)}
             ></input>
           </div>
 
@@ -50,6 +74,8 @@ const RestaurantSignUp = () => {
               type="password"
               placeholder="enter password "
               className="input-field"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
             ></input>
           </div>
 
@@ -58,12 +84,15 @@ const RestaurantSignUp = () => {
               type="password"
               placeholder="enter confirm password "
               className="input-field"
+              value={confirmPassword}
+              onChange={(event) => setConfirmPassword(event.target.value)}
             ></input>
           </div>
 
-
           <div className="input-wrapper">
-            <button type="" className="button">
+            <button type="" className="button" 
+            onClick={handleSignUp}
+            >
               {" "}
               SignUp
             </button>
